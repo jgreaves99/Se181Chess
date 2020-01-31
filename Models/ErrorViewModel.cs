c1285e161ffd28a0e5a3d7c0046b76a5af38b99e
BEN-LAPTOP\Ben <bts48@drexel.edu>
2020-01-31 16:15:01 -0500

    Initial Commit at last

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChessSE181.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
