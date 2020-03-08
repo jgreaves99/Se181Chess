c801a6dd7cc435ef23092b96827bcb7e37c28725
RockonBen <mozartben@gmail.com>
2020-03-08 14:02:26 -0400

    Fixed issues with column h

--- Game/Board.cs
+++ Game/Board.cs
@@ -92,20 +92,20 @@ namespace ChessSE181.Game
             _spaces[2, 0] = new Tile(2, 0, whiteBishop1);
             _spaces[3, 0] = new Tile(3, 0, whiteQueen); 
             _spaces[4, 0] = new Tile(4, 0, whiteKing);
-            _spaces[5, 0] = new Tile(5, 0, whiteRook2); 
+            _spaces[5, 0] = new Tile(5, 0, whiteBishop2); 
             _spaces[6, 0] = new Tile(6, 0, whiteKnight2); 
-            _spaces[7, 0] = new Tile(7, 0, whiteBishop2);
+            _spaces[7, 0] = new Tile(7, 0, whiteRook2);
             
             whiteRook1.setTile(_spaces[0, 0]);
             whiteBishop1.setTile(_spaces[1, 0]);
             whiteKnight1.setTile(_spaces[2, 0]);
             whiteQueen.setTile(_spaces[3, 0]);
             whiteKing.setTile(_spaces[4, 0]);
-            whiteRook2.setTile(_spaces[5, 0]);
+            whiteBishop2.setTile(_spaces[5, 0]);
             whiteBishop2.setTile(_spaces[6, 0]);
-            whiteKnight2.setTile(_spaces[7, 0]);
+            whiteRook2.setTile(_spaces[7, 0]);
             
-            for (var i = 0; i < 7; i++)
+            for (var i = 0; i < 8; i++)
             {
                 var whitePawn = new Pawn("white");
                 _spaces[i, 1] = new Tile(i, 1, whitePawn);
@@ -118,20 +118,20 @@ namespace ChessSE181.Game
             _spaces[2, 7] = new Tile(2,7, blackBishop1);
             _spaces[3, 7] = new Tile(3,7, blackQueen); 
             _spaces[4, 7] = new Tile(4,7, blackKing);
-            _spaces[5, 7] = new Tile(5,7, blackRook2); 
+            _spaces[5, 7] = new Tile(5,7, blackBishop2); 
             _spaces[6, 7] = new Tile(6,7, blackKnight2); 
-            _spaces[7, 7] = new Tile(7,7, blackBishop2);
+            _spaces[7, 7] = new Tile(7,7, blackRook2);
             
             blackRook1.setTile(_spaces[7, 0]);
-            blackBishop1.setTile(_spaces[7, 1]);
-            blackKnight1.setTile(_spaces[7, 2]);
+            blackKnight1.setTile(_spaces[7, 1]);
+            blackBishop1.setTile(_spaces[7, 2]);
             blackQueen.setTile(_spaces[7, 3]);
             blackKing.setTile(_spaces[7, 4]);
-            blackRook2.setTile(_spaces[7, 5]);
-            blackBishop2.setTile(_spaces[7, 6]);
-            blackKnight2.setTile(_spaces[7, 7]);
+            blackBishop2.setTile(_spaces[7, 5]);
+            blackKnight2.setTile(_spaces[7, 6]);
+            blackRook2.setTile(_spaces[7, 7]);
             
-            for (var i = 0; i < 7; i++)
+            for (var i = 0; i < 8; i++)
             {
                 var blackPawn = new Pawn("black");
                 _spaces[i, 6] = new Tile(i, 6, blackPawn);
@@ -139,7 +139,7 @@ namespace ChessSE181.Game
             }
 
 
-            for (var i = 0; i < 7; i++) { 
+            for (var i = 0; i < 8; i++) { 
                 for (var j = 2; j < 6; j++) { 
                     _spaces[i, j] = new Tile(i, j, null); 
                 } 
